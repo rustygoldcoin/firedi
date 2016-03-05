5c9290a182405688b6155177f074248d524c6e06
Joshua Lyle Johnson <josh@ua1.us>
2016-03-05 02:14:21 -0500

    Updated router service with the ability to add specific method calls to route config.

--- src/ulfberht/module/ulfberht/router.php
+++ src/ulfberht/module/ulfberht/router.php
@@ -10,28 +10,47 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
 class router {
-    
+
     private $_request;
     private $_routeCollection;
     private $_defaultController;
-    
+
     public function __construct(request $request) {
         $this->_request = $request;
         $this->_routeCollection = new RouteCollection();
         $this->_defaultController = false;
     }
-    
+
     public function when($path, $controller) {
-        $route = new Route($path, ['controller' => $controller]);
-        $this->_routeCollection->add($path, $route);
+        $this->_addRoute($path, $controller);
         return $this;
     }
-    
+
+    public function get($path, $controller) {
+        $this->_addRoute($path, $controller, 'GET');
+        return $this;
+    }
+
+    public function post($path, $controller) {
+        $this->_addRoute($path, $controller, 'POST');
+        return $this;
+    }
+
+    public function put($path, $controller) {
+        $this->_addRoute($path, $controller, 'PUT');
+        return $this;
+    }
+
+    public function delete($path, $controller) {
+        $this->_addRoute($path, $controller, 'DELETE');
+        return $this;
+    }
+
     public function otherwise($controller) {
         $this->_defaultController = $controller;
         return $this;
     }
-    
+
     public function resolve() {
         $currentRoute = $this->_request->server->get('REQUEST_URI');
         $context = new RequestContext('/');
@@ -47,8 +66,16 @@ class router {
             }
             throw new Exception('Could not find route "' . $currentRoute . '"');
         }
-        
+
         return $parameters;
     }
-    
+
+    private function _addRoute($path, $controller, $method = '') {
+        $route = new Route($path, ['controller' => $controller]);
+        if ($method) {
+            $route->setMethods([$method]);
+        }
+        $this->_routeCollection->add($path, $route);
+    }
+
 }
