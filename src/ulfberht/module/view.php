a02a980e22dcb58b82247631d3e08747c72a0885
Joshua L Johnson <josh@ua1.us>
2016-07-21 02:20:58 -0400

    Rewrote view service.

--- src/ulfberht/module/view.php
+++ src/ulfberht/module/view.php
@@ -13,13 +13,42 @@ use Exception;
 
 class view {
 
-    public function render($viewPath, $viewModel = []){
-        if (!file_exists($viewPath)) {
-            throw new Exception('Could not find view "' . $viewPath . '".');
+    private $_layout = false;
+    private $_view = false;
+
+    public function setLayout($layoutFile) {
+        if (!file_exists($layoutFile)) {
+            throw new Exception('Could not find view layout "' . $layoutFile . '".');
         }
 
-        extract($viewModel);
-        include $viewPath;
+        $this->_layout = $layoutFile;
+    }
+
+    public function render($file){
+        if (!file_exists($file)) {
+            throw new Exception('Could not find view "' . $file . '".');
+        }
+        ob_start();
+        include ($file);
+        if (!$this->_layout) {
+            ob_end_flush();
+        } else {
+            $this->_view = ob_get_contents();
+            ob_end_clean();
+            $this->_renderLayout();
+        }
+    }
+
+    public function view() {
+        if($this->_view) {
+            echo $this->_view;
+        }
+    }
+
+    private function _renderLayout() {
+        ob_start();
+        include ($this->_layout);
+        ob_end_flush();
     }
 
 }
